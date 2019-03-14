680a61d9b761b7e208ce003a6136553c0854b4ff
Arkadiy <dragon_500@mail.ru>
2019-03-14 22:54:35 +0300

    Initial Commit

---
+++ Lab - Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab - Quiz
+//
+//  Created by Arkadiy Grigoryanc on 14/03/2019.
+//  Copyright © 2019 Arkadiy Grigoryanc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
