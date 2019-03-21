55864799631cd49fc52b80dc4e6ba95d68312a09
Arkadiy <dragon_500@mail.ru>
2019-03-21 08:59:28 +0300

    add Lottie lib. and add animation on Main screen

---
+++ Lab - Quiz/Utils.swift
@@ -0,0 +1,18 @@
+//
+//  Utils.swift
+//  Lab - Quiz
+//
+//  Created by Arkadiy Grigoryanc on 21/03/2019.
+//  Copyright © 2019 Arkadiy Grigoryanc. All rights reserved.
+//
+import Foundation
+
+extension Double {
+    func radians() -> Double {
+        return self * Double.pi / 180.0
+    }
+}
+//
+//func DegreesToRadians(_ degrees: Double) -> Double {
+//    return degrees * Double.pi / 180.0
+//}
